928d00e412cd4722671979e46e9a20050c2ed3c2
KomalHundre <123618536+KomalHundre@users.noreply.github.com>
2025-06-16 18:52:56 +0530

    Delete milk/urls.py

--- milk/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('dairy/', include('dairy_collection.urls', namespace='dairy_collection')),
-    path('', lambda request: redirect('dairy_collection:dashboard'), name='root'),
-] 
